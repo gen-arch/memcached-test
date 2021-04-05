1fde202a66a75d6aa3bdac5fcf83dd6b792e92f0
toshifum <toshifum@square-enix.com>
2021-04-05 21:06:27 +0900

    fix : Relaxing the number of concurrent creations

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
 require 'dalli'
 require 'securerandom'
 
-#COUNT=10
-COUNT=10
-MASTER="172.16.0.2:11211"
-REPLICA="172.16.0.3:11211"
-options = { expires_in: 60 }
-$dc = Dalli::Client.new(["#{MASTER}:100", "#{REPLICA}:0"], options)
+INTERVAL  = 0.1
+COUNT     = 10
+MASTER    = "172.16.0.2:11211"
+REPLICA   = "172.16.0.3:11211"
+OPTIONS   = { expires_in: 60 }
+
+$dc = Dalli::Client.new(["#{MASTER}:100", "#{REPLICA}:0"], OPTIONS)
+$m  = Dalli::Client.new(MASTER,  OPTIONS)
+$r  = Dalli::Client.new(REPLICA, OPTIONS)
 
 def put_status
   $dc.reset_stats
@@ -19,17 +22,25 @@ def put_status
   puts "bytes : #{stats[REPLICA]["bytes"]}"
 end
 
+# show status
 put_status
+
+# set keys
+puts "[set] ----------------------------"
 COUNT.times do |i|
   v = SecureRandom.alphanumeric(130)
-  res = $dc.set("key#{i}", v)
+  res = $m.set("key#{i}", v)
   puts "set : key#{i} => #{v}"
+  sleep INTERVAL
 end
 
-puts "values ----------------------------"
-#COUNT.times do |i|
-#  res = $dc.get("key#{i}")
-#  #puts "get : key#{i} => #{res}"
-#end
+# get keys
+puts "[get] ----------------------------"
+COUNT.times do |i|
+  res = $r.get("key#{i}")
+  puts "get : key#{i} => #{res}"
+end
+
+# show status
 put_status
 
